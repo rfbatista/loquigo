9af68ec6a7639016fad5244859c771e96117b418
Renan Batista <renfbatista@gmail.com>
2022-06-13 18:49:29 -0300

    add logger in database

--- database/mongo.go
+++ database/mongo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"loquigo/engine/infrastructure"
+	"loquigo/engine/logger"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -21,21 +22,20 @@ func GetMongoConnection() *mongo.Database {
 	return mongoClient.Database(databaseName)
 }
 
-func NewMongoDb(cfg infrastructure.DatabaseConfig, logger *infrastructure.Logger) MongoDB {
+func NewMongoDb(cfg infrastructure.DatabaseConfig) MongoDB {
 	databaseName = cfg.DbName()
 	uri := fmt.Sprintf("mongodb://%s:%s@%s:%s/?maxPoolSize=20&w=majority",
 		cfg.DbUser(), cfg.DbPassword(), cfg.DbHost(), cfg.DbPort())
-	return MongoDB{uri: uri, dbname: cfg.DbName(), logger: logger}
+	return MongoDB{uri: uri, dbname: cfg.DbName()}
 }
 
 type MongoDB struct {
 	uri    string
 	dbname string
-	logger *infrastructure.Logger
 }
 
 func (m *MongoDB) Connect() {
-	m.logger.Info("Connecting to mongodb")
+	logger.Info("Connecting to mongodb")
 	clientOptions := options.Client().ApplyURI(m.uri)
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
@@ -46,7 +46,7 @@ func (m *MongoDB) Connect() {
 	if err := mongoClient.Ping(context.TODO(), readpref.Primary()); err != nil {
 		panic(err)
 	}
-	m.logger.Info("Connected to MongoDb")
+	logger.Info("Connected to MongoDb")
 }
 
 func (m *MongoDB) Disconnect() {
